ae9920c01d624ce96f09d647b8103de16b745d3a
André Markwalder <andre.markwalder@gmail.com>
2018-06-28 03:59:56 +0200

    Additional tests for models added.

---
+++ tests/test_models.py
@@ -0,0 +1,32 @@
+import unittest
+
+from dynamicdns.models import ConfigProvider, DNSProvider
+
+class TestModels(unittest.TestCase):
+    
+    def testConfigProvider(self):
+
+        class ConfigProviderTest(ConfigProvider):
+            pass
+                
+        config = ConfigProviderTest()
+
+        self.assertRaises(NotImplementedError, config.load)
+        self.assertRaises(NotImplementedError, config.shared_secret, 'abc')
+
+
+    def testDNSProvider(self):
+
+        class DNSProviderTest(DNSProvider):
+            pass
+        
+        dns = DNSProviderTest(None)
+
+        self.assertRaises(NotImplementedError, dns.read, 'abc')
+        self.assertRaises(NotImplementedError, dns.update, 'abc', 'def')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
