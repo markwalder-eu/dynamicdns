efd10f73eccbc30347877691cf06f195f53b2836
André Markwalder <andre.markwalder@gmail.com>
2018-07-01 22:19:02 +0200

    Introduction of pip-tools

--- dynamicdns/__init__.py
+++ dynamicdns/__init__.py
@@ -1,5 +1,4 @@
-from setuptools_scm import get_version
 
 __author__          = "André Markwalder"
 __author_email__    = "andre.markwalder@gmail.com"
-__version__         = get_version()
+__version__         = "0.1.0"
