ccf201959db29f346d8e477901f8ee1762f91324
André Markwalder <andre.markwalder@gmail.com>
2018-08-20 03:05:48 +0200

    Version release dev-20180820-030547.5a8914e

--- dynamicdns/__init__.py
+++ dynamicdns/__init__.py
@@ -1,3 +1,3 @@
-__author__        = "André Markwalder"
-__author_email__  = "andre.markwalder@gmail.com"
-__version__       = "dev-c535a65"
+__author__ = André Markwalder
+__author_email__ = andre.markwalder@gmail.com
+__version__ = dev-20180820-030547.5a8914e
